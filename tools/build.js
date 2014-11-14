7099c22f5f483d9d3e6c645d38a7c51e6adaabfd
Quildreen Motta <quildreen@gmail.com>
2014-11-14 21:08:31 -0200

    Initial work on icons.
    Things are still broken on Windows.

--- tools/build.js
+++ tools/build.js
@@ -1,9 +1,14 @@
+var path = require('path');
 var NwBuilder = require('node-webkit-builder');
+var resource = path.join.bind(path, path.join(__dirname, '..', 'resources'))
+
 var nw = new NwBuilder({
   files: './dist/app/**',
   platforms: ['win', 'linux32', 'linux64', 'osx'],
   version: '0.11.0',
-  buildDir: 'dist'
+  buildDir: 'dist',
+  macIcns: resource('icons/raven.icns'),
+  winIco: resource('icons/raven.ico')
 });
 
 nw.on('log', console.log.bind(console));
