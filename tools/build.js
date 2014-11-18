0120328a5e03f2c54d8b0b525245a9db77bf7ede
Quildreen Motta <quildreen@gmail.com>
2014-11-17 23:10:07 -0200

    Use new flat icon

--- tools/build.js
+++ tools/build.js
@@ -1,14 +1,14 @@
 var path = require('path');
 var NwBuilder = require('node-webkit-builder');
-var resource = path.join.bind(path, path.join(__dirname, '..', 'resources'))
+var platform = path.join.bind(path, path.join(__dirname, '..', 'platform'))
 
 var nw = new NwBuilder({
   files: './dist/app/**',
   platforms: ['win', 'linux32', 'linux64', 'osx'],
   version: '0.11.0',
   buildDir: 'dist',
-  macIcns: resource('icons/raven.icns'),
-  winIco: resource('icons/raven.ico')
+  macIcns: platform('osx/raven.icns'),
+  winIco: platform('windows/raven.ico')
 });
 
 nw.on('log', console.log.bind(console));
